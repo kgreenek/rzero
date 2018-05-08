15cfb5469e73dc8e7d1a79576657566d2768ee78
Kevin Greene <kgreenek@gmail.com>
2018-05-07 23:01:21 -0700

    Update rzero.h to match lib.rs

--- rzero.h
+++ rzero.h
@@ -1,3 +1,2 @@
 #pragma once
-float extract_pitch_raw(float input_ptr, float output_ptr, int length, int input_channels,
-                        int output_channels, double sample_rate);
+float rzero_extract_picth(float *input_ptr, unsigned int length, double sample_rate);
