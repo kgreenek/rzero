64f4dfbe764f116bd88567b61632075cd3a0af32
Kevin Greene <kgreenek@gmail.com>
2018-07-16 23:00:13 -0700

    Wrap YinPitchExtractor in a container struct

--- src/lib.rs
+++ src/lib.rs
@@ -5,15 +5,26 @@ pub mod pitch_extractor;
 
 use pitch_extractor::{PitchExtractor, YinPitchExtractor};
 
+pub struct PitchExtractorContainer {
+    pitch_extractor: YinPitchExtractor<[f32; 1]>,
+}
+
+impl PitchExtractorContainer {
+    fn new() -> Self {
+        Self{pitch_extractor: YinPitchExtractor::<[f32; 1]>::new(100, 150)}
+    }
+}
+
 #[no_mangle]
 pub extern "C" fn rzero_extract_pitch(
-    pitch_extractor_ptr: *mut YinPitchExtractor<[f32; 1]>,
+    pitch_extractor_ptr: *mut PitchExtractorContainer,
     input_ptr: *const f32,
     length: usize,
     sample_rate: f64,
 ) -> f32 {
-    let pitch_extractor = unsafe { &mut *pitch_extractor_ptr };
-    let input = unsafe { std::slice::from_raw_parts(input_ptr, length as usize) };
+    let container = unsafe { &mut *pitch_extractor_ptr };
+    let pitch_extractor = &mut container.pitch_extractor;
+    let input = unsafe { std::slice::from_raw_parts(input_ptr, length) };
     let frames = sample::slice::to_frame_slice::<&[f32], [f32; 1]>(input).unwrap();
     pitch_extractor.add_frames(frames);
     let pitch_samples = pitch_extractor.extract_pitch();
@@ -22,20 +33,18 @@ pub extern "C" fn rzero_extract_pitch(
 }
 
 #[no_mangle]
-pub extern "C" fn rzero_new_pitch_extractor() -> *mut YinPitchExtractor<[f32; 1]> {
-    let _pitch_extractor = unsafe {
-        std::mem::transmute(Box::new(YinPitchExtractor::<[f32; 1]>::new(100, 150)))
+pub extern "C" fn rzero_new_pitch_extractor() -> *mut PitchExtractorContainer {
+    let _container = unsafe {
+        std::mem::transmute(Box::new(PitchExtractorContainer::new()))
     };
-    _pitch_extractor
+    _container
 }
 
 #[no_mangle]
-pub extern "C" fn rzero_free_pitch_extractor(
-    pitch_extractor_ptr: *mut YinPitchExtractor<[f32; 1]>
-) {
-    let _pitch_extractor: Box<YinPitchExtractor<[f32; 1]>> =
+pub extern "C" fn rzero_free_pitch_extractor(pitch_extractor_ptr: *mut PitchExtractorContainer) {
+    let _container: Box<PitchExtractorContainer> =
         unsafe{ std::mem::transmute(pitch_extractor_ptr) };
-    // Drop _pitch_extractor automatically.
+    // Drop _container automatically.
 }
 
 //#[no_mangle]
